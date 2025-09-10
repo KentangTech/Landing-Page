5875a6e0ba17baf245c77d2ef2332fb6e95d4b6e
Moch Islami Al Fatarasya <kentang290505@gmail.com>
2025-09-10 10:50:22 +0700

    Signed-off-by: Moch Islami Al Fatarasya <kentang290505@gmail.com>

---
+++ pages/api/data/direksi.js
@@ -0,0 +1,11 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch('https://adminweb.grahasaranagresik.com//api/direksi');
+    const data = await response.json();
+
+    res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate');
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Gagal fetch data dari Laravel' });
+  }
+}
